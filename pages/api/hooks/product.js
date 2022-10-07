769c90bd71450d4233e1e9b7fb54a1fa170cce28
m.morasaeiyan <m.morasaeiyan@irantalent.com>
2022-10-07 23:23:10 +0330

    add isr

--- pages/api/hooks/product.js
+++ pages/api/hooks/product.js
@@ -5,12 +5,14 @@ import {SIGNATURE_HEADER_NAME,isValidSignature} from "@sanity/webhook"
 const handler = async(req,res) =>{
     try {
         const signature = req.headers[SIGNATURE_HEADER_NAME].toString();
+        console.log('moras1')
         if(!isValidSignature(
             JSON.stringify(req.body),
             signature,
             process.env.SANITY_WEBHOOK_SECRET
         )) return res.status(401).json({msg:"Invalid requset"})
         const {slug} = req.body;
+        console.log('moras2')
         await res.revalidate(`/products/isr/${slug}`);
         await res.revalidate(`products/isr`)
         console.log('slug',slug)
