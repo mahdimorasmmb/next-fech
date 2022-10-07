74013dbfb39917f31be3c381e2f5c8959feead69
m.morasaeiyan <m.morasaeiyan@irantalent.com>
2022-10-07 19:46:37 +0330

    iknit

---
+++ pages/products/ssr/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { sanityClient } from "../../../utils/sanity/client";
+import Link from "next/link";
+
+const Products = ({ products }) => {
+  return (
+    <div>
+      {products.map((product) => (
+        <Link href={`/products/ssr/${product.slug}`} key={product._id}>
+          <a>
+            <p>{product.title}</p>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
+
+export const getServerSideProps = async () => {
+  const products = await sanityClient.fetch(
+    '*[_type=="product" && defined(slug.current) && !(_id in path("drafts.**"))]{...,"slug":slug.current}'
+  );
+  return {
+    props: { products },
+  };
+};
